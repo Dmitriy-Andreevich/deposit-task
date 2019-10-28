473ea16e51f32689447ac6bfee253564f4500949
Andreevich <donsy2002@gmail.com>
2019-10-28 20:58:05 +0200

    DEPO-1 create project core

---
+++ src/main/java/space/meduzza/deposittask/DepositTaskApplication.java
@@ -0,0 +1,13 @@
+package space.meduzza.deposittask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepositTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/space/meduzza/deposittask/DepositTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package space.meduzza.deposittask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepositTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
